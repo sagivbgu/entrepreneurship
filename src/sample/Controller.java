780d9733f83143f56473d8498b2f0cdad4bca935
liaru <souseisekiftw@gmail.com>
2020-01-14 15:11:34 +0200

    Trying to GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class Controller {
+    @FXML private Text actiontarget;
+    public Stage thisStage;
+
+    public Controller(Stage scene){
+        thisStage = scene;
+    }
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Let's go!");
+    }
+
+    public void setStage (Stage stage){
+        thisStage = stage;
+    }
+
+    public void showStage(String fxml){
+        thisStage.setTitle("Title");
+        thisStage.show();
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,18 +2,49 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class Main extends Application {
+    private Parent root;
+    private Controller controller;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+//        Stage stage;
+//        root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//
+//        Scene scene = new Scene(root, 300, 275);
+//        primaryStage.setTitle("RateFit");
+//        primaryStage.setScene(scene);
+//        controller = new Controller(primaryStage);
+//        primaryStage.show();
+//        stage = primaryStage;
+        Label welcomeMsg = new Label("Hello Shlomi, welcome to RateFit");
+        Button btn = new Button("Start a workout");
+        GridPane root = new GridPane();
+        Scene scene = new Scene(root,300,300);
+        root.addRow(0, welcomeMsg);
+        root.addRow(1, btn);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("RateFit");
         primaryStage.show();
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        } );
     }
 
 
